127d39b1795c57c093974e9edfd020f5d61f1836
rotoyouoi <rotoyouoi@yandex.ru>
2022-12-16 11:00:54 +0500

    Добавьте файлы проекта.

---
+++ Model/PartialClass/ServicePartial.cs
@@ -0,0 +1,69 @@
+﻿using System.Windows;
+
+namespace CarService.Model.PartialClass
+{
+    class ServicePartial
+    {
+        public string DiscountText
+        {
+            get
+            {
+                if (Discount == 0) || Discount == null)
+                    return "";
+                else
+                    return $"* скидка {Discount * 100} %";
+            }
+        }
+
+        public double CostWithDiscount
+        {
+            get
+            {
+                if (Discount == 0 || Discount == null)
+                {
+                    return (double)CostWithDiscount;
+                }
+                else
+                {
+                    var CostWithDiscount = (double)Cost * 1.00 - Discount);
+                    return CostWithDiscount.Value;
+                }
+            }
+        }
+        public string TotalCost
+        {
+            get
+            {
+                if(DiscountText == 0 || Discount == null)
+                {
+                    return $"{Cost:N2} рублей за {DurationInSeconds / 60} минут";
+                }
+                else
+                {
+                    return $"{CostWithDiscount:N2} рублей за {DurationInSeconds / 60}минут";
+                }
+            }
+        }
+        public Visibility DiscountVisibility
+        {
+            get
+            {
+                if (DiscountText == 0 || DiscountText == null)
+                    return Visibility.Collapsed;
+                else
+                    return Visibility.Visible;
+            }
+        }
+        public string BackColor
+        {
+            get
+            {
+                if (Discount == 0 || Discount == null)
+                    return "#FFFFE1";
+                else
+                    return "#D1FFD1";
+            }
+        }
+
+    }   
+}
